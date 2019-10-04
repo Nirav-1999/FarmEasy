cd532f4a86d3897f35572070d4c6edf0f195d1df
Nirav <niravjain1709@gmail.com>
2019-10-05 03:33:50 +0530

    Rainfall_Year_View

--- graph/views.py
+++ graph/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import pandas as pd
 
 # Create your views here.
+class RainfallYearView(APIView):
+    def post(self,request):
+        data = pd.read_csv('../data/rainfall.csv')
+        search = request.query_params["region"]
+        data = data.loc[data['SUBDIVISION'] == search]
+        data = data['ANNUAL'][-15:]
+        return Response({'Rainfall_2001-2015':list(data)})
