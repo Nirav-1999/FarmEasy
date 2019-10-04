9d0c0f26c48471b6b9d15f1c4db3ae2c7ae3f606
Nirav <niravjain1709@gmail.com>
2019-10-05 05:24:54 +0530

    hosting

--- FarmEasy/settings.py
+++ FarmEasy/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn farmeasy.wsgi --log-file -
+web: gunicorn FarmEasy.wsgi --log-file -
