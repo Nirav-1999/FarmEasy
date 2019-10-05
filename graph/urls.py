9566deefc487db78aaff567d52ac071079f36232
Nirav <niravjain1709@gmail.com>
2019-10-05 16:14:16 +0530

    PredictView

--- graph/urls.py
+++ graph/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
     path('monthly_rainfall',views.MonthlyRainfallAccToRegion.as_view(),name='monthly_rainfall'),
 
-    path('yearly_rain/',views.RainfallYearView.as_view(),name = 'yearly-rain')
+    path('yearly_rain/',views.RainfallYearView.as_view(),name = 'yearly-rain'),
+
+    path('predicted_rain/',views.PredictedResultView.as_view(),name='predictions'),
 
 ]
--- graph/views.py
+++ graph/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import pandas as pd
+import pickle
+import numpy as np
 
 # Create your views here.
 class RainfallYearView(APIView):
@@ -43,4 +45,73 @@ class MonthlyRainfallAccToRegion(APIView):
 
 
     
+class PredictedResultView(APIView):
+    def post(self,request):
+        with open("./data/model","rb") as f:
+            model = pickle.load(f)
+        with open("./data/standard_scalar","rb") as f:
+            some = pickle.load(f)
+        sc_X=some[0]
+        sc_y = some[1]
+        y_pred = {}
+        values = [
+            'SUBDIVISION_ANDAMAN & NICOBAR ISLANDS',
+            'SUBDIVISION_ARUNACHAL PRADESH',
+            'SUBDIVISION_ASSAM & MEGHALAYA',
+            'SUBDIVISION_BIHAR',
+            'SUBDIVISION_CHHATTISGARH',
+            'SUBDIVISION_COASTAL ANDHRA PRADESH',
+            'SUBDIVISION_COASTAL KARNATAKA',
+            'SUBDIVISION_EAST MADHYA PRADESH',
+            'SUBDIVISION_EAST RAJASTHAN',
+            'SUBDIVISION_EAST UTTAR PRADESH',
+            'SUBDIVISION_GANGETIC WEST BENGAL',
+            'SUBDIVISION_GUJARAT REGION',
+            'SUBDIVISION_HARYANA DELHI & CHANDIGARH',
+            'SUBDIVISION_HIMACHAL PRADESH',
+            'SUBDIVISION_JAMMU & KASHMIR',
+            'SUBDIVISION_JHARKHAND',
+            'SUBDIVISION_KERALA',
+            'SUBDIVISION_KONKAN & GOA',
+            'SUBDIVISION_LAKSHADWEEP',
+            'SUBDIVISION_MADHYA MAHARASHTRA',
+            'SUBDIVISION_MATATHWADA',
+            'SUBDIVISION_NAGA MANI MIZO TRIPURA',
+            'SUBDIVISION_NORTH INTERIOR KARNATAKA',
+            'SUBDIVISION_ORISSA',
+            'SUBDIVISION_PUNJAB',
+            'SUBDIVISION_RAYALSEEMA',
+            'SUBDIVISION_SAURASHTRA & KUTCH',
+            'SUBDIVISION_SOUTH INTERIOR KARNATAKA',
+            'SUBDIVISION_SUB HIMALAYAN WEST BENGAL & SIKKIM',
+            'SUBDIVISION_TAMIL NADU',
+            'SUBDIVISION_TELANGANA',
+            'SUBDIVISION_UTTARAKHAND',
+            'SUBDIVISION_VIDARBHA',
+            'SUBDIVISION_WEST MADHYA PRADESH',
+            'SUBDIVISION_WEST RAJASTHAN',
+            'SUBDIVISION_WEST UTTAR PRADESH',
+            'months_APR',
+            'months_AUG',
+            'months_DEC',
+            'months_FEB',
+            'months_JAN',
+            'months_JUL',
+            'months_JUN',
+            'months_MAR',
+            'months_MAY',
+            'months_NOV',
+            'months_OCT',
+            'months_SEP']
+        pred_val = [0]*len(values)
+        search = request.data["region"]
+        for i in range(12):
+            for j in range(len(values)):
+                if values[j]==search:
+                    pred_val[j] = 1
+                    break
+            pred_val[i+36] = 1
+            y_pred[values[i+36]]=(sc_y.inverse_transform(model.predict(sc_X.transform(np.array(np.array([pred_val]))))))
+            print(y_pred)
 
+        return Response(y_pred)
