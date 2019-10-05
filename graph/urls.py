43b0cf04df3d2e9963c5cca10b123c6fbac2512f
Chinmay Deo <chinmay.deo7@yahoo.com>
2019-10-05 16:14:25 +0530

    Monthly_rainfall

--- graph/urls.py
+++ graph/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('regions/',views.GetRegions.as_view(),name='regions')
+    path('regions/',views.GetRegions.as_view(),name='regions'),
+    path('monthly_rainfall',views.MonthlyRainfallAccToRegion.as_view(),name='monthly_rainfall'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -9,11 +9,28 @@ class GetRegions(APIView):
     def get(self, request):
         df=pd.read_csv('./data/rainfall in india 1901-2015.csv')
         reg_list=df['SUBDIVISION'].unique()
-        reg_dict={}
-        reg_dict['Regions']=reg_list
         return Response({
             'Regions': reg_list
         })
+
+
+class MonthlyRainfallAccToRegion(APIView):
+    def post(self,request):
+        region=request.data['Region']
+        year=request.data['Year']
+        df=pd.read_csv('./data/rainfall in india 1901-2015.csv')
+        df=df.loc[df['SUBDIVISION']==region]
+        df=df.loc[df['YEAR']==year]
+        df.drop('ANNUAL',axis=1,inplace=True)
+        df.drop('Jan-Feb',axis=1,inplace=True)
+        df.drop('Mar-May',axis=1,inplace=True)
+        df.drop('Jun-Sep',axis=1,inplace=True)
+        df.drop('Oct-Dec',axis=1,inplace=True)
+        return Response({
+            'Monthly_Rainfall': df
+        })
+
+
 
 
 
