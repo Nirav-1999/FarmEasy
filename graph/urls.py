bcf2dbf2f669ee40d88341f05d4a6e9cafd7c7a2
Chinmay Deo <chinmay.deo7@yahoo.com>
2019-10-05 15:28:11 +0530

    Added Regions

--- graph/urls.py
+++ graph/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graph/',include('graph.urls'))
+    path('regions/',views.GetRegions.as_view(),name='regions')
 ]
--- graph/views.py
+++ graph/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView,View
+from rest_framework.response import Response
+from rest_framework import serializers,generics
+import pandas as pd 
 
 # Create your views here.
+class GetRegions(APIView):
+    def get(self, request):
+        df=pd.read_csv('./data/rainfall in india 1901-2015.csv')
+        reg_list=df['SUBDIVISION'].unique()
+        reg_dict={}
+        reg_dict['Regions']=reg_list
+        return Response({
+            'Regions': reg_list
+        })
+
+
+
+    
+
