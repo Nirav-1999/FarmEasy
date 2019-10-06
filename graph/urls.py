859b2e79210255b2a7d9d134fe519416e25c6fb1
Chinmay Deo <chinmay.deo7@yahoo.com>
2019-10-06 12:31:10 +0530

    Final commit

--- graph/urls.py
+++ graph/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('yearly_rain/',views.RainfallYearView.as_view(),name = 'yearly-rain'),
     path('test/',views.webhookTest.as_view(),name='test'),
     path('range/',views.Range.as_view(),name='Range'),
-    path('pdf/',views.pdfformat.as_view(),name='pdf')
+    path('pdf/',views.pdfformat.as_view(),name='pdf'),
     
 
     path('predicted_rain/',views.PredictedResultView.as_view(),name='predictions'),
